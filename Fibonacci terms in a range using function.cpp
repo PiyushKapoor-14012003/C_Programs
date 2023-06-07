0a3a8ff941e0819da1fd939a6937f24b5a0c3065
Piyush Kapoor <2k22cscys2213057@gmail.com>
2023-06-07 10:32:38 +0530

    Add files via upload

---
+++ Fibonacci terms in a range using function.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void fibonacci(int start,int end)
+{
+	int f1=0,f2=1,f3;
+	if(start==1)
+	{
+		printf("0 1 ");
+	}
+	for(int i=3;i<=end;i++)
+	{
+		f3=f1+f2;
+		if(i>=start)
+		printf("%d ",f3);
+		f1=f2;
+		f2=f3;
+	}
+}
+int main()
+{
+	int start,end;
+	printf("Enter range of fibonacci terms to be printed: ");
+	scanf("%d %d",&start,&end);
+	fibonacci(start,end);
+}
